8d950e95ba4edb2fc70ed98092da12aee907a074
Andrew Volokh <laturo.volokh@yandex.ru>
2016-11-29 23:11:08 +0400

    Update ex1(a).cs

--- ex1(a).cs
+++ ex1(a).cs
@@ -8,7 +8,6 @@ namespace tsett
 			int size = int.Parse(Console.ReadLine());
 			int[] array = new int[size];
 			for (int i = 0; i < size;i++){
-
 				array[i] = int.Parse(Console.ReadLine());
 			}
 			Console.Write("Массив: ");
