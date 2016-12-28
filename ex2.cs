f43e8769e6d8c9f4d5a7c42db20d641119129ff5
Andrew Volokh <laturo.volokh@yandex.ru>
2016-12-28 13:32:16 +0400

    Update ex2.cs

--- ex2.cs
+++ ex2.cs
@@ -6,24 +6,24 @@ namespace ееее
 	{
 		public static void Main(string[] args)
 		{
-			
 
-			int buf;
-			string s=Console.ReadLine();
+
+		
+			string s = Console.ReadLine();
 			char[] b = s.ToCharArray();
-			
+			int element;
 			Console.Write("Массив до замены: ");
-			const char[] check={'1','2','3','4','5','6','7','8','9','0'};
-			for (int i = 0; i < b.Length;i++) {
-				for(int j=0;j<al.Length();j++){
-				if(b[i]==check[j]){
-				b[i]='$';
-				}
-				}
+			for (int i = 0; i < b.Length; i++)
+			{
+				element = b[i] - '0';
+				if (element >= 0 && element <= 9)
+					b[i] = '$';
 			}
 			Console.Write("\r\nМассив после замены: ");
-			for (char i in b) {
+			foreach (char i in b)
+			{
 				Console.Write(i);
+
 			}
 		}
 	}
